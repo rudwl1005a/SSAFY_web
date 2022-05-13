55a9256fa9a532b24350aedb8506fe4d3258cddc
Min kyungdae <rudwl1005@naver.com>
2022-05-13 10:44:48 +0900

    2022-05-13 Vue (connect springboot)

--- VueCLI/portal-example/vue.config.js
+++ VueCLI/portal-example/vue.config.js
@@ -2,4 +2,5 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+  outputDir: "C:/Users/com/git/SSAFY_Spring/PortalExample/src/main/resources/static"
 })
