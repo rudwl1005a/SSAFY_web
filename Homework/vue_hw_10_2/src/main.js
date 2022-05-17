d3202001dece6eedd9d8f5b1f2112adb30c3cf5d
Min kyungdae <rudwl1005@naver.com>
2022-05-17 22:34:34 +0900

    2022-05-17 vue (hw8)

---
+++ Homework/vue_hw_10_2/src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App.vue'
+import store from "./store/index.js"
+import router from "./router/index.js"
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+  router,
+  store,
+}).$mount('#app')
